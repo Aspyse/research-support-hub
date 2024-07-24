8d9dacfa777b624003d38cd5f8abf8dec18c5e5f
LourenzBani <lourenz_jhay_baniqued@dlsu.edu.ph>
2024-07-24 17:31:58 +0800

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -1,12 +1,18 @@
 // Import the functions you need from the SDKs you need
 // https://firebase.google.com/docs/reference/js
 //import 'dotenv/config'
+
+//firebase-app
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+
+//firebase-auth
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
-import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js';
-import { collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
 import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js'
+import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js';
+
+//firebase-firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import { collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
 import { query, where, getDocs, updateDoc, doc } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js'
 
 // TODO: Add SDKs for Firebase products that you want to use
